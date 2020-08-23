c79fc6d4b38ebfba2a7de1a68d6368d56a742093
Neylson Crepalde <neylsoncrepalde@gmail.com>
2020-08-23 12:26:46 -0300

    provider with s3 backend

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,12 +1,10 @@
 # provider
-data "terraform_remote_state" "network" {
-  backend = "s3"
-
-  config {
-    bucket = var.tf_state_bucket
-    key    = var.tf_state_key
-    region = var.aws_region
-  }
+terraform {
+    backend "s3" {
+      bucket = "xxxxxxxxxxxx"
+      key    = "xxxxxxxxxxx"
+      region = "xxxxxxxxxxx"
+    }
 }
 
 provider "aws" {
