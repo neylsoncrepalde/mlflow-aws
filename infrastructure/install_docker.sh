4e441e607abd30a087c0171774839e19263b3cb5
Neylson Crepalde <neylsoncrepalde@gmail.com>
2020-10-07 23:04:39 -0300

    dont git pull in user data

--- infrastructure/install_docker.sh
+++ infrastructure/install_docker.sh
@@ -3,4 +3,3 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install docker.io -y
 sudo service docker start
 sudo usermod -a -G docker ubuntu
-git clone https://github.com/neylsoncrepalde/mlflow-aws.git
